f3e6b6e88c920e4507577970c20a316bc3658864
whlprogram <long2451774200@gmail.com>
2017-04-23 00:35:59 +0800

    2014.04.23
    LeetCode 504 转化为七进制 日常刷题

--- 504Base7/main.cpp
+++ 504Base7/main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
-
+#include <string>
+#include <stdlib.h>
 using namespace std;
-
+class Solution {
+public:
+    string convertToBase7(int num) {
+        if(num == 0)
+            return "0";
+        string res = "";
+        int NUM = abs(num);//È¡¾ø¶ÔÖµ
+        while(NUM != 0){
+            int tmp = NUM%7;
+            NUM = NUM/7;
+            res = to_string(tmp) + res;
+        }
+        if(num < 0)
+            return "-"+res;
+        return res;
+    }
+};
 int main()
 {
-    cout << "Hello world!" << endl;
+    string res1 = "123";
+    string res2 = "456";
+    cout << res2+res1 << endl;
+    cout << "-"+res2 << endl;
+    Solution a;
+    int num = 0;
+    cin >> num;
+    cout << a.convertToBase7(num) << endl;
     return 0;
 }
