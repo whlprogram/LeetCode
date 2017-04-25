ec4e2a35af5ad110666e7126350c7f9d3ab94e3d
whlprogram <long2451774200@gmail.com>
2017-04-25 20:36:40 +0800

    2017.04.25
    LeetCode 563 二叉树的各节点左右子树绝对差的和

--- 563BinaryTreeTilt/main.cpp
+++ 563BinaryTreeTilt/main.cpp
@@ -1,7 +1,34 @@
+//求出各个节点 左右子树的差 的绝对值，将这些绝对值求和并返回
+//左右子树的差 = | 左子树所有节点的值的和 - 右子树所有节点的值的和 |
 #include <iostream>
-
+#include <stdlib.h>
 using namespace std;
 
+
+class Solution {
+public:
+    // Definition for a binary tree node.
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    };
+    int tilt = 0;
+    int findTilt(TreeNode* root) {
+        sum(root);
+        return tilt;
+    }
+    int sum(TreeNode* root){//自顶向下求解
+        if(root == nullptr)
+            return 0;
+        int left = sum(root->left);//左子树各节点和
+        int right = sum(root->right);//右子树各节点和
+        tilt += left>right ? left-right : right-left;
+        //tilt += abs(left - right);
+        return left + right + root->val;//返回左右子树各节点和
+    }
+};
 int main()
 {
     cout << "Hello world!" << endl;
