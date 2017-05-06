b1e1521ecf4a4346aa9189c502af0ed3139d22dd
whlprogram <long2451774200@gmail.com>
2017-05-06 22:15:54 +0800

    2017.05.06
    占坑

---
+++ 551StudentAttendanceRecordI/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    bool checkRecord(string s) {
+
+    }
+};
+int main()
+{
+    Solution a;
+    string s;
+    cin >> s;
+    cout << a.checkRecord(s) << endl;
+    return 0;
+}
