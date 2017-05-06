6d75e27ffc84cc113cd66d9c4a7f7babc000bd18
whlprogram <long2451774200@gmail.com>
2017-05-07 00:08:17 +0800

    2017.05.06
    LeetCode 551 根据学生出勤记录判断是否应获奖金

--- 551StudentAttendanceRecordI/main.cpp
+++ 551StudentAttendanceRecordI/main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 #include <string>
 using namespace std;
-class Solution {
+class Solution {//‘≠Ã‚ https://leetcode.com/problems/student-attendance-record-i/#/description
 public:
     bool checkRecord(string s) {
-
+        if(s.size() <= 1)
+            return true;
+        int Acount = 0;
+        int ContinuousLcount = 0;
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == 'A'){
+               Acount++;
+               ContinuousLcount = 0;
+            }
+            else if(s[i] == 'L')
+                ContinuousLcount++;
+            else
+                ContinuousLcount = 0;
+            if(Acount>1 || ContinuousLcount>2)
+                return false;
+        }
+        return true;
     }
 };
 int main()
@@ -12,6 +28,9 @@ int main()
     Solution a;
     string s;
     cin >> s;
+    cout << s.size() << endl;
+    //cout << true << endl; //1
+    //cout << false << endl; //0
     cout << a.checkRecord(s) << endl;
     return 0;
 }
